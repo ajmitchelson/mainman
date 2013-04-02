d50bb0dd9f5e5d99aa30db49057ca773c30f6914
mitcheja <mitcheja@mainman.home.lan>
2013-04-02 16:14:19 +0100

    committing changes in /etc after apt run
    Package changes:
    -bind9-host 1:9.7.3.dfsg-1~squeeze9
    +bind9-host 1:9.7.3.dfsg-1~squeeze10
    -dnsutils 1:9.7.3.dfsg-1~squeeze9
    +dnsutils 1:9.7.3.dfsg-1~squeeze10
    -host 1:9.7.3.dfsg-1~squeeze9
    +host 1:9.7.3.dfsg-1~squeeze10
    -libbind9-60 1:9.7.3.dfsg-1~squeeze9
    +libbind9-60 1:9.7.3.dfsg-1~squeeze10
    -libdns69 1:9.7.3.dfsg-1~squeeze9
    +libdns69 1:9.7.3.dfsg-1~squeeze10
    -libisc62 1:9.7.3.dfsg-1~squeeze9
    -libisccc60 1:9.7.3.dfsg-1~squeeze9
    -libisccfg62 1:9.7.3.dfsg-1~squeeze9
    +libisc62 1:9.7.3.dfsg-1~squeeze10
    +libisccc60 1:9.7.3.dfsg-1~squeeze10
    +libisccfg62 1:9.7.3.dfsg-1~squeeze10
    -liblwres60 1:9.7.3.dfsg-1~squeeze9
    +liblwres60 1:9.7.3.dfsg-1~squeeze10
    -roundcube 0.7.1-1~bpo60+1
    -roundcube-core 0.7.1-1~bpo60+1
    -roundcube-mysql 0.7.1-1~bpo60+1
    +roundcube 0.7.2-9~bpo60+1
    +roundcube-core 0.7.2-9~bpo60+1
    +roundcube-mysql 0.7.2-9~bpo60+1

--- roundcube/debian-db.php
+++ roundcube/debian-db.php
@@ -3,7 +3,7 @@
 ## database access settings in php format
 ## automatically generated from /etc/dbconfig-common/roundcube.conf
 ## by /usr/sbin/dbconfig-generate-include
-## Wed, 30 Jan 2013 13:53:17 +0000
+## Tue, 02 Apr 2013 16:14:00 +0100
 ##
 ## by default this file is managed via ucf, so you shouldn't have to
 ## worry about manual changes being silently discarded.  *however*,
